74c7c9b569df8fc40c8e3725cb3f63fdc859b874
ito_kyohei <ito_kyohei@cyberagent.co.jp>
2017-04-26 19:25:05 -0700

    Create project

---
+++ RxDisplayLink/RxDisplayLink.h
@@ -0,0 +1,19 @@
+//
+//  RxDisplayLink.h
+//  RxDisplayLink
+//
+//  Created by Kyohei Ito on 2017/04/26.
+//  Copyright © 2017年 Kyohei Ito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxDisplayLink.
+FOUNDATION_EXPORT double RxDisplayLinkVersionNumber;
+
+//! Project version string for RxDisplayLink.
+FOUNDATION_EXPORT const unsigned char RxDisplayLinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxDisplayLink/PublicHeader.h>
+
+
