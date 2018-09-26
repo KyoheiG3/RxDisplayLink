9c87ef7d3965ce7727e54603ca50851f80be7a3e
Kyohei Ito <je.suis.kyohei+github@gmail.com>
2018-09-26 10:45:29 +0900

    Fix for swift4.2

--- RxDisplayLink/RxDisplayLink.swift
+++ RxDisplayLink/RxDisplayLink.swift
@@ -20,7 +20,7 @@ public extension Reactive where Base: CADisplayLink {
      - Parameter fps: Frames per second. Default and max are 60.
      - Returns: Observable of CADisplayLink.
      */
-    public static func link(to runloop: RunLoop = .main, forMode mode: RunLoopMode = .commonModes, fps: Int = Base.maximumFps) -> Observable<CADisplayLink> {
+    public static func link(to runloop: RunLoop = .main, forMode mode: RunLoop.Mode = .common, fps: Int = Base.maximumFps) -> Observable<CADisplayLink> {
         return RxDisplayLink(to: runloop, forMode: mode, fps: fps).asObservable()
     }
 }
@@ -29,7 +29,7 @@ public final class RxDisplayLink: ObservableType {
     public typealias E = CADisplayLink
     
     private let runloop: RunLoop
-    private let mode: RunLoopMode
+    private let mode: RunLoop.Mode
     private let fps: Int
     private var observer: AnyObserver<E>?
     
@@ -37,7 +37,7 @@ public final class RxDisplayLink: ObservableType {
         observer?.onNext(link)
     }
     
-    public init(to runloop: RunLoop, forMode mode: RunLoopMode, fps: Int) {
+    public init(to runloop: RunLoop, forMode mode: RunLoop.Mode, fps: Int) {
         self.runloop = runloop
         self.mode = mode
         self.fps = fps
